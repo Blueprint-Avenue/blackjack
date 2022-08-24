ec7155a387bd2dc7efbfe6f3f600505d22b9a0b1
Samiya <skmcintosh20@gmail.com>
2022-08-24 16:01:22 -0700

    Added a cards array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
 let secondCard = 11;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -15,7 +16,7 @@ function startGame() {
 
 function renderGame() {
 	sumEl.textContent = "Sum: " + sum;
-	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+	cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
 	if (sum <= 20) {
 		message = "Do you want to draw a new card?";
 	} else if (sum === 21) {
@@ -33,6 +34,6 @@ function newCard() {
 	let card = 7;
 
 	sum += card;
-
+	cards.push(card);
 	renderGame();
 }
