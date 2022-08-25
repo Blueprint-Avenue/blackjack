6a5fd909f08f3bc19e201576f8f08773850c1fa8
Samiya <skmcintosh20@gmail.com>
2022-08-25 12:28:00 -0700

    added a player/ completed game

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ let sum = 0;
 let hasBlackJack = false;
 let isAlive = false;
 let message = "";
+let player = {
+	name: "Mac",
+	chips: 1185,
+};
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
 	let randomNumber = Math.floor(Math.random() * 13) + 1;
